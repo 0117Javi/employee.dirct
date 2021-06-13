c5fc92466f0d8cf8a8b46606a9f407f6867a329a
jj espinoza <j.j.espinozagomez@gmail.com>
2021-06-13 17:41:35 -0500

    bootstrapin'

--- empdirectory/src/App.js
+++ empdirectory/src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import React from "react";
+import Header from "./components/header";
 
 const App = () => {
-  return <div className="Container">hello</div>;
+  return (
+    <div className="Container">
+      <Header />
+    </div>
+  );
 };
 
 export default App;

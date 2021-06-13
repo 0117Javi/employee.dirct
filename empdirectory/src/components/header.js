680cc7185b180faee403206ead5203be83965777
jj espinoza <j.j.espinozagomez@gmail.com>
2021-06-13 17:15:16 -0500

    src files

---
+++ empdirectory/src/components/header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <header className="jumbotron jumbotron-fluid">
+      <div className="container">
+        <h1 className="display-4">Bootcamp Spot</h1>
+        <text className="lead">Employee Directory</text>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
